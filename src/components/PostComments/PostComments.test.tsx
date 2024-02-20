520dd7e40c9d5509e6f7b8fc7ad4b7d91d0dd936
Marcelo Oliveira <marcelo@exemplo.com>
2024-02-20 20:09:37 -0300

    teste

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,24 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve acrescentar dois comentários ao comentario-li', () => {
+        render(<PostComment />);
+
+        fireEvent.change(screen.getByTestId('comentario-textarea'), {
+            target: {
+                value: 'Testando o primeiro comentário'
+            }
+        });
+        fireEvent.click(screen.getByTestId('comentario-button'));
+
+        fireEvent.change(screen.getByTestId('comentario-textarea'), {
+            target: {
+                value: 'Testando o segundo comentário'
+            }
+        });
+        fireEvent.click(screen.getByTestId('comentario-button'));
+
+        expect(screen.getAllByTestId('comentario-li')).toHaveLength(2);
+    })
 });
